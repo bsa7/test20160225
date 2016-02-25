991ad2fd1fc2346ecd31b0a6e5f7b804d4c3eff1
Sergeij Belevskij <bbelevskij@gmail.com>
2016-02-25 12:06:31 +0500

    add devise and some tests for root, users

--- db/migrate/20160225055303_add_email_field_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailFieldToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :email, :string
-  end
-end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :user do
     name { Faker::Name.name }
     full_description { Faker::Name.title }
-    email {Faker::Internet.email}
+    email {Faker::Internet.safe_email}
+    password {Faker::Internet.password(10)}
   end
 end
